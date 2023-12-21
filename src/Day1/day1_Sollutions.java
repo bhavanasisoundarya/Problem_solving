d16cf3f83260d23276af1fbf8a2cf369266309ff
bhavanasisoundarya <116268639+bhavanasisoundarya@users.noreply.github.com>
2023-12-21 19:05:40 +0530

    Initial commit

---
+++ src/Day1/day1_Sollutions.java
@@ -0,0 +1,48 @@
+package Day1;
+
+public class day1_Sollutions {
+
+
+	public static void main(String[] args) {
+	//...............................//
+		//question one
+//		int s = 3;
+//		for(int i=1; i<=s; i++) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+		
+		
+		//................................................// 
+		// question two
+//		int s = 5;
+//		for(int i=0; i<=s; i++) {
+//			for(int j=0; j<s-i-1; j++) {
+//				System.out.print(" ");
+//			}
+//		
+//		for(int k=0; k<=i; k++) {
+//			if(k==i) {
+//				System.out.print("*");
+//			}else {
+//				System.out.print("* ");
+//			}
+//		
+//		}
+//		System.out.println();
+//		}
+//		
+//............................//
+		// third question//
+		int s = 3;
+		for(int i=1; i<=s; i++) {
+			for(int j=1; j<=s; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
